b9d47071a75647a3e17d50228c3653c86ad12fd2
Mateus Slovak Silva <89343643+MateusSlovakSilva@users.noreply.github.com>
2022-02-21 21:12:50 -0300

    Add files via upload
    exercício 01 python3

---
+++ ex001.py
@@ -0,0 +1,6 @@
+msg = ('Exercicio 001')
+print(msg)
+nome = input('Qual é seu nome ? ')
+print('É um prezer em te conhecer {}!'.format(nome))
+ano = input('Qual ano voce nasceu ? ')
+print('Seu nome é {}, voce nasceu no ano de {}.'.format(nome,ano))
