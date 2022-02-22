4f17dc168c7153143a9f1a4cbff76fe8e62698f4
Mateus Slovak Silva <89343643+MateusSlovakSilva@users.noreply.github.com>
2022-02-22 19:06:52 -0300

    Exercício de python
    fazendo calculo entre 2 números inteiros

---
+++ ex002.py
@@ -0,0 +1,5 @@
+msg = ('Execicio 002 calculo')
+numer1 =int (input('Digite um valor: '))
+numero2 =int (input('Digite outro valor: '))
+soma = numer1 + numero2
+print('A soma entre {0} e {1} vale {2}: '.format(numer1, numero2,soma))
