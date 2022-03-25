924366dd508c0efe381b3d8edaeea211f361bb79
Mateus Slovak Silva <89343643+MateusSlovakSilva@users.noreply.github.com>
2022-03-25 11:02:31 -0300

    exercício 015
    Aluguel de carro.

---
+++ ex015.py
@@ -0,0 +1,6 @@
+msg = ('exercício 015')
+print(msg)
+dias = int(input('Quantos dias alugados ? '))
+km = float(input('Quantos Km rodados? '))
+pago = dias * 60 + (km * 0.15)
+print('O total a pagar é de R${:.2f}'.format(pago))
