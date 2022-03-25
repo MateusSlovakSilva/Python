9bb12ca2336f78a6b3f1f39d5cceac8b1f95498b
Mateus Slovak Silva <89343643+MateusSlovakSilva@users.noreply.github.com>
2022-03-25 10:58:44 -0300

    exercício 012
    Usando porcentagem, dando desconto no valor do produto em 5%

---
+++ ex012.py
@@ -0,0 +1,5 @@
+msg = ('exercício 012')
+print(msg)
+preço = float(input('Qual é preço do Produto? R$ '))
+novo = preço - (preço * 5 / 100)
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preço, novo))
