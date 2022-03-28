4cbefea3ad84421e3d3330473b0fb180d6afe0de
Mateus Slovak Silva <89343643+MateusSlovakSilva@users.noreply.github.com>
2022-03-27 21:28:50 -0300

    exercício 008 conversor de medidas
    Digite uma medida em metros que vai mostrar quantos centímetros tem e quantos milímetros tem

---
+++ aula 08.py
@@ -0,0 +1,7 @@
+msg = ('Aula 08 aprendendo importa biblioteca')
+print(msg)
+from math import sqrt, floor
+import emoji
+num = int(input('Digite um numero: '))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))
